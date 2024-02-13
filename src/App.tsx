5bba61dc3c883839cb0b93eee4b6a224f878a7e5
zoroQT20 <155709657+zoroQT20@users.noreply.github.com>
2024-02-13 15:46:37 +0800

    FAQS and Contact form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import './App.css'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import Faqs from './pages/Faqs'; 
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-
-
   return (
     <>
-  <Router>
-      <Navbar/>
-      <Routes>
-        {/* <Route path="/form" element={<Form/>} />
-        <Route path="/faqs" element={<FAQs/>} />
-        <Route path="/contact" element={<Contact/>} /> */}
-        <Route path="/" element={<Home/>} />
-      </Routes>
-      <Footer/>
-    </Router>
-</>
-  )
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/faqs" element={<Faqs />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
