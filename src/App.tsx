31c8a36c72f50f8d4d5470f1461ada6e4d140b31
LanceEnr <lancemikaelo.enriquez@gmail.com>
2024-02-26 07:21:33 +0800

    rif tracker, side nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,55 @@
-import React, { FC } from 'react';
-import './App.css';
-import Home from './user-side/pages/Home';
-import Faqs from './user-side/pages/Faqs';
-import Contact from './user-side/pages/Contact';
-import Navbar from './user-side/components/Navbar';
-import NavbarAdmin from './admin/components/Navbar';
-import Footer from './user-side/components/Footer';
-import RIF from './user-side/pages/RIF';
-import RiskInputForm from'./admin/pages/RiskInputForm';
-import DocumentGrid from './admin/pages/DocumentGrid';
+import React, { FC } from "react";
+import "./App.css";
+import Home from "./user-side/pages/Home";
+import Faqs from "./user-side/pages/Faqs";
+import Contact from "./user-side/pages/Contact";
+import Navbar from "./user-side/components/Navbar";
+import NavbarAdmin from "./admin/components/Navbar";
+import Footer from "./user-side/components/Footer";
+import RIF from "./user-side/pages/RIF";
+import RiskInputForm from "./admin/pages/RiskInputForm";
+import DocumentGrid from "./admin/pages/DocumentGrid";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RifTracker from "./admin/pages/RifTracker";
+import SideNavbar from "./admin/components/SideNavbar";
 
 const App: FC = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/admin/*" element={
-            <>
-              <NavbarAdmin />
-        <Routes>
-            <Route path="risk" element={<RiskInputForm />} />  {/* Ganto maglagay sa LINK /admin/risk */}
-            <Route path="grid" element={<DocumentGrid />} />   {/* Ganto maglagay sa LINK /admin/grid */}
-        </Routes>
-        <Footer />
-            </>
-          } />
+          <Route
+            path="/admin/*"
+            element={
+              <>
+                <SideNavbar />
+                <Routes>
+                  <Route path="risk" element={<RiskInputForm />} />{" "}
+                  {/* Ganto maglagay sa LINK /admin/risk */}
+                  <Route path="grid" element={<DocumentGrid />} />{" "}
+                  {/* Ganto maglagay sa LINK /admin/grid */}
+                  <Route path="tracker" element={<RifTracker />} />
+                </Routes>
+              </>
+            }
+          />
 
-          <Route path="/*" element={
-            <>
-              <Navbar />
-              <Routes>
-                <Route path="/FAQS" element={<Faqs />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/Contact" element={<Contact />} />
-                <Route path="/RIF" element={<RIF />} />
-              </Routes>
-              <Footer />
-            </>
-          } />
+          <Route
+            path="/*"
+            element={
+              <>
+                <Navbar />
+                <Routes>
+                  <Route path="/FAQS" element={<Faqs />} />
+                  <Route path="/" element={<Home />} />
+                  <Route path="/Contact" element={<Contact />} />
+                  <Route path="/RIF" element={<RIF />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </>
--- src/user-side/pages/Home.tsx
+++ src/user-side/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Home: React.FC = () => {
   return (
@@ -14,28 +14,52 @@ const Home: React.FC = () => {
               <span className="text-yellow-500">Platform</span>
             </h1>
 
-            <p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-              <br className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
-              <br className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
-            </p>  
-            <a href="/RIF" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-yellow-500 hover:bg-primary-800 focus:ring-4 focus:ring-yellow-300">
+            <p className="mt-6 mb-8 text-lg sm:mb-12">
+              Dictum aliquam porta in condimentum ac integer
+              <br className="hidden md:inline lg:hidden" />
+              turpis pulvinar, est scelerisque ligula sem
+              <br className="hidden md:inline lg:hidden" />
+              turpis pulvinar, est scelerisque ligula sem
+            </p>
+            <a
+              href="/RIF"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-yellow-500 hover:bg-primary-800 focus:ring-4 focus:ring-yellow-300"
+            >
               Get started
-              <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
             </a>
-            <a href="/Faqs" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-yellow-500 border-2 border-yellow-500 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+            <a
+              href="/Faqs"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-yellow-500 border-2 border-yellow-500 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+            >
               Frequently Asked Questions
-            </a> 
+            </a>
           </div>
           <div className="hidden lg:flex lg:mt-10 lg:col-span-5 ">
             <div className="relative rounded-lg overflow-hidden">
-              <img src="https://c0.wallpaperflare.com/preview/453/640/998/building-philippines-ust.jpg" alt="mockup" className="w-full h-full"/>
+              <img
+                src="https://c0.wallpaperflare.com/preview/453/640/998/building-philippines-ust.jpg"
+                alt="mockup"
+                className="w-full h-full"
+              />
               <div className="absolute inset-0 bg-gradient-to-r from-yellow-500 via-yellow-400 to-yellow-300 opacity-50"></div>
             </div>
-          </div>                
+          </div>
         </div>
       </section>
     </>
-  ) 
-}
+  );
+};
 
 export default Home;
