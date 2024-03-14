8786ca2e755b55bd871194de59ad6a0c76e5b6b2
Lance <lancemikaelo.enriquez@gmail.com>
2024-03-14 14:17:40 +0800

    fixes

--- src/admin/pages/RifTrackerTable.tsx
+++ src/admin/pages/RifTrackerTable.tsx
@@ -134,7 +134,6 @@ const RifTrackerTable: React.FC = () => {
                         className="w-5 h-5"
                         aria-hidden="true"
                         fill="currentColor"
-                        viewbox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -224,7 +223,6 @@ const RifTrackerTable: React.FC = () => {
                     className="w-5 h-5"
                     aria-hidden="true"
                     fill="currentColor"
-                    viewbox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -305,7 +303,6 @@ const RifTrackerTable: React.FC = () => {
                     className="w-5 h-5"
                     aria-hidden="true"
                     fill="currentColor"
-                    viewbox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -386,7 +383,6 @@ const RifTrackerTable: React.FC = () => {
                     className="w-5 h-5"
                     aria-hidden="true"
                     fill="currentColor"
-                    viewbox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -468,7 +464,6 @@ const RifTrackerTable: React.FC = () => {
                     className="w-5 h-5"
                     aria-hidden="true"
                     fill="currentColor"
-                    viewbox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -549,7 +544,6 @@ const RifTrackerTable: React.FC = () => {
                     className="w-5 h-5"
                     aria-hidden="true"
                     fill="currentColor"
-                    viewbox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
