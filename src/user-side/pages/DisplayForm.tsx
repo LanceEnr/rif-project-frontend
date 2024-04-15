d63bb88608fcdbc8fece974de86de47cc2d1eeee
Lance <lancemikaelo.enriquez@gmail.com>
2024-04-15 15:31:17 +0800

    working code, able to fetch from database

--- src/user-side/pages/DisplayForm.tsx
+++ src/user-side/pages/DisplayForm.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import WebForm from "../components/WebForm";
+import RiskFormsTable from "../components/RiskFormsTable";
 
 const DisplayForm: React.FC = () => {
   return (
-    <div className="max-w-screen-xl mx-auto px-4  min-h-screen my-24">
+    <div className="max-w-screen-xl mx-auto px-4 min-h-screen my-24">
       <div className="flex flex-col items-right">
         <h2 className="font-bold text-5xl mt-5 tracking-tight">Web Form</h2>
-
         <hr className="h-px my-8 border-yellow-500 border-2" />
       </div>
-      <WebForm />
+      <RiskFormsTable />
     </div>
   );
 };
