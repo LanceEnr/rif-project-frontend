e2041bd341b7c10f6dcd68e7a4b61dff706cf2cb
Lance <lancemikaelo.enriquez@gmail.com>
2024-04-22 14:36:15 +0800

    prerequisites frontend fixes

--- src/user-side/pages/Esignature.tsx
+++ src/user-side/pages/Esignature.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Dropdown } from "flowbite-react";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Esignature: React.FC = () => {
   return (
@@ -11,43 +13,6 @@ const Esignature: React.FC = () => {
           <hr className="h-px my-8 border-yellow-500 border-2" />
         </div>{" "}
         <div className="grid grid-cols-8 pt-3 sm:grid-cols-10">
-          <div className="relative my-4 w-56 sm:hidden">
-            <input
-              className="peer hidden"
-              type="checkbox"
-              name="select-1"
-              id="select-1"
-            />
-            <label className="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-yellow-500 peer-checked:ring">
-              Accounts{" "}
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="pointer-events-none absolute right-0 top-3 ml-auto mr-5 h-4 text-slate-700 transition peer-checked:rotate-180"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M19 9l-7 7-7-7"
-              />
-            </svg>
-            <ul className="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-yellow-500 hover:text-white">
-                Accounts
-              </li>
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-yellow-500 hover:text-white">
-                Team
-              </li>
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-yellow-500 hover:text-white">
-                Others
-              </li>
-            </ul>
-          </div>
-
           <div className="col-span-2 hidden sm:block">
             <ul>
               <Link to="/prerequisites">
@@ -67,6 +32,33 @@ const Esignature: React.FC = () => {
               <h1 className="py-2 text-2xl font-semibold">
                 E-Signature Upload
               </h1>
+              <div className=" col-span-8 sm:hidden">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Navigation
+                </label>
+                <Dropdown
+                  label=""
+                  inline
+                  dismissOnClick={false}
+                  renderTrigger={() => (
+                    <button
+                      id="dropdownActionButton"
+                      data-dropdown-toggle="dropdownAction"
+                      className="inline-flex w-full py-2.5 items-center  bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100  rounded-lg text-sm px-3 "
+                      type="button"
+                    >
+                      <div className="flex justify-between w-full">
+                        <span>E-signature Upload</span>
+                        <MdKeyboardArrowDown className="h-5 w-5" />
+                      </div>
+                    </button>
+                  )}
+                >
+                  <Dropdown.Item as={Link} to="/prerequisites">
+                    Prerequisites
+                  </Dropdown.Item>
+                </Dropdown>
+              </div>
             </div>
             <hr className="mt-4 mb-8" />
             <div className="mb-10">
--- src/user-side/pages/Prerequisites.tsx
+++ src/user-side/pages/Prerequisites.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 import { FaTrashCan } from "react-icons/fa6";
+import { Dropdown } from "flowbite-react";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Prerequisites: React.FC = () => {
   return (
@@ -13,43 +15,6 @@ const Prerequisites: React.FC = () => {
           <hr className="h-px my-8 border-yellow-500 border-2" />
         </div>
         <div className="grid grid-cols-8 pt-3 sm:grid-cols-10">
-          <div className="relative my-4 w-56 sm:hidden">
-            <input
-              className="peer hidden"
-              type="checkbox"
-              name="select-1"
-              id="select-1"
-            />
-            <label className="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-yellow-500 peer-checked:ring">
-              Accounts{" "}
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="pointer-events-none absolute right-0 top-3 ml-auto mr-5 h-4 text-slate-700 transition peer-checked:rotate-180"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M19 9l-7 7-7-7"
-              />
-            </svg>
-            <ul className="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-yellow-500 hover:text-white">
-                Accounts
-              </li>
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-yellow-500 hover:text-white">
-                Team
-              </li>
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-yellow-500 hover:text-white">
-                Others
-              </li>
-            </ul>
-          </div>
-
           <div className="col-span-2 hidden sm:block">
             <ul>
               <li className="mt-5 cursor-pointer border-l-2 border-l-yellow-500 px-2 py-2 font-semibold text-yellow-500 transition hover:border-l-yellow-500 hover:text-yellow-500">
@@ -67,6 +32,33 @@ const Prerequisites: React.FC = () => {
           <div className="col-span-8 overflow-hidden rounded-xl sm:bg-yellow-100 sm:px-8 sm:shadow-md">
             <div className="pt-4">
               <h1 className="py-2 text-2xl font-semibold">Prerequisites</h1>
+              <div className=" col-span-8 sm:hidden">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Navigation
+                </label>
+                <Dropdown
+                  label=""
+                  inline
+                  dismissOnClick={false}
+                  renderTrigger={() => (
+                    <button
+                      id="dropdownActionButton"
+                      data-dropdown-toggle="dropdownAction"
+                      className="inline-flex w-full py-2.5 items-center  bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100  rounded-lg text-sm px-3 "
+                      type="button"
+                    >
+                      <div className="flex justify-between w-full">
+                        <span>Prerequisities</span>
+                        <MdKeyboardArrowDown className="h-5 w-5" />
+                      </div>
+                    </button>
+                  )}
+                >
+                  <Dropdown.Item as={Link} to="/esignature">
+                    E-signature Upload
+                  </Dropdown.Item>
+                </Dropdown>
+              </div>
             </div>
             <hr className="mt-4 mb-8" />
 
@@ -75,15 +67,15 @@ const Prerequisites: React.FC = () => {
                 <div className="grid gap-4 mb-4 sm:grid-cols-1">
                   <div>
                     <label
-                      htmlFor="username"
+                      htmlFor="unit"
                       className="block mb-2 text-sm font-medium text-gray-900"
                     >
                       Administrative/Academic Unit
                     </label>
                     <input
                       type="text"
-                      name="username"
-                      id="username"
+                      name="unit"
+                      id="unit"
                       className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                       placeholder="e.g. College of Education"
                     />
@@ -93,7 +85,7 @@ const Prerequisites: React.FC = () => {
                     <div className="flex justify-between items-start mb-2">
                       <div>
                         <label
-                          htmlFor="username"
+                          htmlFor="internal-stakeholder"
                           className="block mb-2 text-sm font-medium text-gray-900"
                         >
                           Internal Client/Stakeholder
@@ -116,8 +108,8 @@ const Prerequisites: React.FC = () => {
                     <div className="relative flex items-center mb-2">
                       <input
                         type="text"
-                        name="username"
-                        id="username"
+                        name="internal-stakeholder"
+                        id="internal-stakeholder"
                         className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                         placeholder="e.g. Academic Staff"
                       />
@@ -131,7 +123,7 @@ const Prerequisites: React.FC = () => {
                     <div className="flex justify-between items-start mb-2">
                       <div>
                         <label
-                          htmlFor="username"
+                          htmlFor="external-stakeholder"
                           className="block mb-2 text-sm font-medium text-gray-900"
                         >
                           External Client/Stakeholder
@@ -154,8 +146,8 @@ const Prerequisites: React.FC = () => {
                     <div className="relative flex items-center mb-2">
                       <input
                         type="text"
-                        name="username"
-                        id="username"
+                        name="external-stakeholder"
+                        id="external-stakeholder"
                         className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                         placeholder="e.g. Parents & Guardians"
                       />
