4223266d0457c0a4258919850c6df92827a27a39
zoroQT20 <155709657+zoroQT20@users.noreply.github.com>
2024-05-20 14:27:26 +0800

    p not final admin side navbar name

--- src/admin/components/SideNavbar.tsx
+++ src/admin/components/SideNavbar.tsx
@@ -132,7 +132,7 @@ const SideNavbar: React.FC = () => {
                           fill="currentColor"
                           viewBox="0 0 18 18"
                         >
-                          <path d="M15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783ZM6 2h6a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2Zm7 5a1 1 0 0 1-2 0V7h2Z" />
+                          <path d="M15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783ZM6 2h6a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2Zm7 5H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Z" />
                           <path d="M1 18h16a1 1 0 0 0 1-1v-6h-4.439a.99.99 0 0 0-.908.6 3.978 3.978 0 0 1-7.306 0 .99.99 0 0 0-.908-.6H0v6a1 1 0 0 0 1 1Z" />
                         </svg>
                       </div>
@@ -165,7 +165,7 @@ const SideNavbar: React.FC = () => {
                           fill="currentColor"
                           viewBox="0 0 18 18"
                         >
-                          <path d="M15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783ZM6 2h6a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2Zm7 5a1 1 0 0 1-2 0V7h2Z" />
+                          <path d="M15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783ZM6 2h6a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2Zm7 5a1 1 0 1 1-2 0V7h2Z" />
                           <path d="M1 18h16a1 1 0 0 0 1-1v-6h-4.439a.99.99 0 0 0-.908.6 3.978 3.978 0 0 1-7.306 0 .99.99 0 0 0-.908-.6H0v6a1 1 0 0 0 1 1Z" />
                         </svg>
                       </div>
@@ -206,21 +206,13 @@ const SideNavbar: React.FC = () => {
 
                 {isAuthenticated && user ? (
                   <Dropdown
-                    label={
-                      <Avatar
-                        alt="User settings"
-                        img="https://static.generated.photos/vue-static/face-generator/landing/wall/14.jpg"
-                        rounded
-                      />
-                    }
+                    label={<Avatar alt="User settings" img="https://static.generated.photos/vue-static/face-generator/landing/wall/14.jpg" rounded />}
                     arrowIcon={false}
                     inline
                   >
                     <Dropdown.Header>
                       <span className="block text-sm">{`${user.firstname} ${user.lastname}`}</span>
-                      <span className="block truncate text-sm font-medium">
-                        {user.email}
-                      </span>
+                      <span className="block truncate text-sm font-medium">{user.email}</span>
                     </Dropdown.Header>
                     <Dropdown.Item>Dashboard</Dropdown.Item>
                     <Dropdown.Item>Settings</Dropdown.Item>
@@ -229,23 +221,15 @@ const SideNavbar: React.FC = () => {
                   </Dropdown>
                 ) : (
                   <Dropdown
-                    label={
-                      <button className="py-2 px-4 text-white rounded bg-yellow-500">
-                        Menu
-                      </button>
-                    }
+                    label={<button className="py-2 px-4 text-white rounded bg-yellow-500">Menu</button>}
                     arrowIcon={false}
                     inline
                   >
                     <Dropdown.Item>
-                      <Link to="/login" className="block w-full text-left">
-                        Login
-                      </Link>
+                      <Link to="/login" className="block w-full text-left">Login</Link>
                     </Dropdown.Item>
                     <Dropdown.Item>
-                      <Link to="/register" className="block w-full text-left">
-                        Register
-                      </Link>
+                      <Link to="/register" className="block w-full text-left">Register</Link>
                     </Dropdown.Item>
                   </Dropdown>
                 )}
@@ -354,6 +338,48 @@ const SideNavbar: React.FC = () => {
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Reports</span>
               </Link>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                  />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
+              </a>
             </li>
           </ul>
         </div>
