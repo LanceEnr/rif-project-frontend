9032d65e3e7b84b98ff95df2b3c40ebd33108113
zoroQT20 <155709657+zoroQT20@users.noreply.github.com>
2024-05-21 09:14:00 +0800

    mobile resposiveness

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -171,10 +171,19 @@ const Login = () => {
                       Forgot password?
                     </a>
                   </div>
-                  <ReCAPTCHA
-                    sitekey="6LdqSOIpAAAAABh9QlokKcKE3OdJLFslH9M5alo2"
-                    onChange={handleCaptchaChange}
-                  />
+                  <div
+                    className="recaptcha-container"
+                    style={{
+                      transform: "scale(0.85)",
+                      transformOrigin: "0 0",
+                      maxWidth: "100%",
+                    }}
+                  >
+                    <ReCAPTCHA
+                      sitekey="6LdqSOIpAAAAABh9QlokKcKE3OdJLFslH9M5alo2"
+                      onChange={handleCaptchaChange}
+                    />
+                  </div>
                   <button
                     type="submit"
                     className="w-full text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
