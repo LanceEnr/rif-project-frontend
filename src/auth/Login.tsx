f67627747681db9a55ba9fbe53f8dd2be665996f
Lance <lancemikaelo.enriquez@gmail.com>
2024-06-01 15:34:42 +0800

    frontend fixes

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -48,7 +48,7 @@ const Login = () => {
     e.preventDefault();
 
     if (!email.endsWith("@ust.edu.ph")) {
-      setError("Please use a ust email");
+      setError("Please use your university email");
       return;
     }
 
@@ -166,7 +166,7 @@ const Login = () => {
                         setEmail(e.target.value)
                       }
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                      placeholder="user@gmail.com"
+                      placeholder="user@ust.edu.ph"
                       required
                     />
                   </div>
--- src/auth/Register.tsx
+++ src/auth/Register.tsx
@@ -98,7 +98,7 @@ const Register = () => {
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                      placeholder="name@company.com"
+                      placeholder="user@ust.edu.ph"
                       required
                     />
                   </div>
