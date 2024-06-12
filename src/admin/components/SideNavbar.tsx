524d43f1ac5b883ae567669458bbd4fe5a887ba5
Lance <lancemikaelo.enriquez@gmail.com>
2024-06-12 09:27:08 +0800

    frontend fixes for navbars

--- src/admin/components/SideNavbar.tsx
+++ src/admin/components/SideNavbar.tsx
@@ -200,9 +200,11 @@ const SideNavbar: React.FC = () => {
                                 !notification.isRead ? "bg-yellow-100" : ""
                               }
                             >
-                              <div>{notification.message}</div>
-                              <div className="text-xs text-gray-400">
-                                {formatDate(notification.timestamp)}
+                              <div>
+                                <div>{notification.message}</div>
+                                <div className="text-xs flex justify-left text-gray-400">
+                                  {formatDate(notification.timestamp)}
+                                </div>
                               </div>
                             </Dropdown.Item>
                           ))
@@ -305,14 +307,18 @@ const SideNavbar: React.FC = () => {
                   className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
                 >
                   <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    viewBox="0 0 16 16"
                     fill="currentColor"
-                    viewBox="0 0 20 18"
+                    height="1em"
+                    width="1em"
                   >
-                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                    <path
+                      fill="currentColor"
+                      d="M0 16h8V0H0v16zM5 2h2v2H5V2zm0 4h2v2H5V6zm0 4h2v2H5v-2zM1 2h2v2H1V2zm0 4h2v2H1V6zm0 4h2v2H1v-2zm8-5h7v1H9zm0 11h2v-4h3v4h2V7H9z"
+                    />
                   </svg>
+
                   <span className="flex-1 ms-3 whitespace-nowrap">Units</span>
                 </Link>
               </li>
@@ -472,12 +478,15 @@ const SideNavbar: React.FC = () => {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
                   fill="currentColor"
-                  viewBox="0 0 20 18"
+                  height="1em"
+                  width="1em"
                 >
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                  <path
+                    fill="currentColor"
+                    d="M0 16h8V0H0v16zM5 2h2v2H5V2zm0 4h2v2H5V6zm0 4h2v2H5v-2zM1 2h2v2H1V2zm0 4h2v2H1V6zm0 4h2v2H1v-2zm8-5h7v1H9zm0 11h2v-4h3v4h2V7H9z"
+                  />
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Units</span>
               </Link>
